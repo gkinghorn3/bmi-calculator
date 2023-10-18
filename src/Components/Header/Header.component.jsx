407e227027bcf9bb7987a45e7fe9f15b0beb8607
Grant <gkinghorn3@icloud.com>
2023-10-18 16:37:23 +0100

    first

---
+++ src/Components/Header/Header.component.jsx
@@ -0,0 +1,13 @@
+import './Header.styles.scss';
+
+const HeaderContainer = () => {
+    return(
+       <div className='header-container'>
+         <h1>BMI Calculator</h1>
+       </div>
+    )
+    
+
+}
+
+export default HeaderContainer;
