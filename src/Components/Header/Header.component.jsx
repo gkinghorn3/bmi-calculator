25b13a81959da4745aa1923726b035a412b76e6f
Grant <gkinghorn3@icloud.com>
2023-10-18 16:59:23 +0100

    m

--- src/Components/Header/Header.component.jsx
+++ src/Components/Header/Header.component.jsx
@@ -1,13 +1,20 @@
-import './Header.styles.scss';
+import "./Header.styles.scss";
 
 const HeaderContainer = () => {
-    return(
-       <div className='header-container'>
-         <h1>BMI Calculator</h1>
-       </div>
-    )
-    
-
-}
+  return (
+    <div className="header-container">
+      <div className="header-text-content">
+        <h1> Body Mass Index Calculator</h1>
+        <p className='header-para'>
+          
+          Better understand your weight in relation to your height using our
+          body mass index (BM) calculator. While BMI is not the sole determinant
+          of a healthy weight, it offers a valuable starting point to evaluate
+          your overall health and well-being.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default HeaderContainer;
